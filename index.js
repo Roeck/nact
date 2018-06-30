e890532bc8dafae5f650fd92cec8fc8481eb470a
Roeck <rons9n@gmail.com>
2018-06-30 18:10:04 -0400

    nodemon setup

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const app = express();
 
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
-	clientSecret: keys.googleClientSecret
+	clientSecret: keys.googleClientSecret,
 	callbackURL: '/auth/google/callback'
     }, 
-    accessToken => {
-  	console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+    	console.log('access token', accessToken);
+    	console.log('refresh token', refreshToken);
+    	console.log('profile:', profile);
+  	
     }
   )
 );
@@ -23,6 +26,8 @@ app.get('/auth/google',
 
       })
     );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
