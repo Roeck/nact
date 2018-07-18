7a5a193bd94afe39cadcff4105106bedd745679a
Roeck <rons9n@gmail.com>
2018-07-17 20:29:25 -0400

    Link tags

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
+ 
 class Header extends Component {
 	renderContent() {
 		switch (this.props.auth) {
@@ -10,16 +11,19 @@ class Header extends Component {
               return <li><a href="/auth/google">Login With Google</a></li>;
               	
            default:
-              return <li><a>Logout</a></li>;
+              return <li><a href="/api/logout">Logout</a></li>;
 		}
 	}
 	render() {
 		return (
 			<nav>
 			    <div className="nav-wrapper">
-			      <a className="left brand-logo">
+			      <Link 
+			       to={this.props.auth ? '/surveys' : '/'}
+			       className="left brand-logo">
+			        
 			        Nact
-			      </a>
+			      </Link>
 				      <ul className="right">
 				        {this.renderContent()}
 				      </ul>  
