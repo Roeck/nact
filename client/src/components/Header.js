a1d4d93bfaee56ff2281bb5430e3c4d87d219090
Roeck <rons9n@gmail.com>
2018-07-19 20:13:31 -0400

    stripe tokens

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
  
 class Header extends Component {
 	renderContent() {
 		switch (this.props.auth) {
            case null:
               return;
+
            case false:
               return <li><a href="/auth/google">Login With Google</a></li>;
               	
            default:
-              return <li><a href="/api/logout">Logout</a></li>;
+              return [
+                 <li><Payments /></li>,
+                 <li><a href="/api/logout">Logout</a></li>
+              ];
+              
 		}
 	}
+
 	render() {
 		return (
 			<nav>
