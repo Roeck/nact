33a5c3087a70c073385edb75ac2b7bba6942940a
Roeck <rons9n@gmail.com>
2018-08-03 20:36:52 -0400

    User improvement

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
 	return (
-		
-		<div class="fixed-action-btn">
-		  <Link to="/surveys/new" class="btn-floating btn-large red">
-		    <i class="large material-icons">add_circle_outline</i>
-		  </Link>
+		<div>
+		    <SurveyList />
+		    <div class="fixed-action-btn">
+		       <Link to="/surveys/new" class="btn-floating btn-large red">
+		         <i class="large material-icons">add_circle_outline</i>
+		       </Link>
+		    </div>
 		</div>
-		
 	);
 };
 
